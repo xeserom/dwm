0033b7eab6af750e7e278005ef01500da1a8f160
SekretAgentMan <banana.man@keemail.me>
2022-07-14 20:15:16 +0000

    changed mod key

--- config.def.h
+++ config.def.h
@@ -45,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 
 #define TAGKEYS(KEY,TAG)												\
 	{1, {{MODKEY, KEY}},								view,           {.ui = 1 << TAG} },	\
